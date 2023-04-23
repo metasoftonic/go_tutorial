3da23e1d4e753ef1f41dfc587ef88c0ab5d41bb7
devopsLord <ozoanihenry@gmail.com>
2023-04-23 11:43:25 +0100

    changes

--- session_two/greetings/greetings.go
+++ session_two/greetings/greetings.go
@@ -1,8 +1,8 @@
-package greetings;
+package greetings
 
 import "fmt"
 
 func Hello(name string) string {
-	message := fmt.Sprint("Hi, %v. Nice to meet  you", name)
+	message := fmt.Sprintf("Hi, %v. Nice to meet you", name)
 	return message
 }
